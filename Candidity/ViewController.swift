b5bc4b844b5529f7bdb87b89678a3c259863f47e
Changyuan Lin <coreballium@outlook.com>
2019-03-17 21:26:00 -0600

    Begin setting up capture session and permissions

--- Candidity/ViewController.swift
+++ Candidity/ViewController.swift
@@ -7,15 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet var oneTouchGesture: UITapGestureRecognizer!
     @IBOutlet var twoTouchGesture: UITapGestureRecognizer!
     
+    let captureSession = AVCaptureSession()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        switch (AVCaptureDevice.authorizationStatus(for: .video),
+                AVCaptureDevice.authorizationStatus(for: .audio)){
+            case (.authorized, .authorized):  // User already authorized camera access
+                print("Already authorized!")
+            case (.notDetermined, .authorized):  // User not yet asked for camera access
+                AVCaptureDevice.requestAccess(for: .video) { granted in
+                    if granted {
+                        print("Video granted")
+                    }
+                }
+            case (.authorized, .notDetermined):
+                AVCaptureDevice.requestAccess(for: .audio) { granted in
+                    if granted {
+                        print("Audio granted")
+                    }
+                }
+            case (.notDetermined, .notDetermined):
+                AVCaptureDevice.requestAccess(for: .video) { granted in
+                    if granted {
+                        print("Video granted")
+                    }
+                }
+                AVCaptureDevice.requestAccess(for: .audio) { granted in
+                    if granted {
+                        print("Audio granted")
+                    }
+                }
+            default:
+                print("Either video or audio was not allowed!")
+        }
+    }
+    
+    // What happens if we begin setting up before permissions are granted?
+    // Perhaps add a preview to test
+    
+    func setupCaptureSession() {
+        captureSession.beginConfiguration()
+        let videoDevice = AVCaptureDevice.default(for: .video)
+        guard
+            let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!),
+            captureSession.canAddInput(videoDeviceInput)
+            else { return }
+        captureSession.addInput(videoDeviceInput)
     }
 
     @IBAction func oneTouch(_ sender: UITapGestureRecognizer) {
