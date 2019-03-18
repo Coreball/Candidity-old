8f44c58e0cd13a39620c157d2b958bad0dc7f19e
Changyuan Lin <coreballium@outlook.com>
2019-03-17 20:38:53 -0600

    Test gestures and permissions for camera/microphone

--- Candidity/ViewController.swift
+++ Candidity/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var oneTouchGesture: UITapGestureRecognizer!
+    @IBOutlet var twoTouchGesture: UITapGestureRecognizer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func oneTouch(_ sender: UITapGestureRecognizer) {
+        print("ABDSUHFE")
+    }
+    
+    @IBAction func twoTouch(_ sender: UITapGestureRecognizer) {
+        print("ABCSD")
+    }
+    
 }
 
